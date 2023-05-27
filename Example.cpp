754664bfb56c57d660285902bd1d04277f8a80c5
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-27 13:19:42 -0700

    Services

--- Example.cpp
+++ Example.cpp
@@ -28,9 +28,48 @@ public:
     }
 };
 
+struct SomeService {
+    virtual void Increment()  = 0;
+    virtual int  GetCounter() = 0;
+};
+
+class SomeServiceImpl : public SomeService {
+    int counter = 0;
+
+public:
+    SomeServiceImpl() = default;
+    SomeServiceImpl(int startCount) : counter(startCount) {}
+
+    void Increment() override { counter++; }
+    int  GetCounter() override { return counter; }
+};
+
 int main() {
+    auto x = 69;
+
     DependencyInjection::DIContainer container;
 
+    container.RegisterService<SomeService, SomeServiceImpl>();
+
+    auto& someServiceReference = container.Get<SomeService>();
+    someServiceReference->Increment();
+    someServiceReference->Increment();
+    std::cout << "someServiceReference->GetCounter() = " << someServiceReference->GetCounter()
+              << std::endl;
+
+    // But we can also reset it so it makes a new instance
+    container.ResetService<SomeService>();
+    std::cout << "someServiceReference->GetCounter() = " << someServiceReference->GetCounter()
+              << std::endl;  // should be 0 now, new instance of SomeServiceImpl
+
+    // Finally, I want to support passing constructor arguments in both of these scenarios:
+    // 1 - on RegisterService
+    // 2 - on ResetService
+    // container.RegisterService<SomeService, SomeServiceImpl>(42);
+    // // or on ResetService
+    // container.ResetService<SomeService>(42);
+
+    // previous examples should still work, of course
     container.Register<CoolInterface, CoolClass>();
     container.Register<RadInterface, RadClass, int>();
 
--- include/dependency_injection.h
+++ include/dependency_injection.h
@@ -10,7 +10,10 @@ namespace DependencyInjection {
     class DIContainer {
         using DeleterFunc = std::function<void(void*)>;
 
-        std::unordered_map<std::type_index, std::function<std::pair<void*, DeleterFunc>(std::any)>>
+        std::unordered_map<
+            std::type_index, std::pair<
+                                 std::function<std::pair<void*, DeleterFunc>(std::any)>,
+                                 std::unique_ptr<void, DeleterFunc>>>
             _factories;
 
     public:
@@ -18,26 +21,63 @@ namespace DependencyInjection {
 
         template <typename Base, typename Derived, typename... Args>
         void Register() {
-            _factories[typeid(Base)] = [](std::any args) {
-                auto     argsTuple = std::any_cast<std::tuple<Args...>>(args);
-                Derived* raw_ptr   = new Derived(std::apply(
-                    [](auto&&... args) { return Derived(std::forward<decltype(args)>(args)...); },
-                    argsTuple
-                ));
-                return std::make_pair(static_cast<void*>(raw_ptr), [](void* ptr) {
-                    delete static_cast<Derived*>(ptr);
-                });
-            };
+            _factories[typeid(Base)] = {
+                [](std::any args) {
+                    auto     argsTuple = std::any_cast<std::tuple<Args...>>(args);
+                    Derived* raw_ptr   = new Derived(std::apply(
+                        [](auto&&... args) {
+                            return Derived(std::forward<decltype(args)>(args)...);
+                        },
+                        argsTuple
+                    ));
+                    return std::make_pair(static_cast<void*>(raw_ptr), [](void* ptr) {
+                        delete static_cast<Derived*>(ptr);
+                    });
+                },
+                nullptr};
         }
 
         template <typename Base, typename... Args>
         std::unique_ptr<Base, DeleterFunc> New(Args&&... args) {
             auto it = _factories.find(typeid(Base));
             if (it != _factories.end()) {
-                auto [raw_ptr, deleter] = it->second(std::make_tuple(std::forward<Args>(args)...));
+                auto [raw_ptr, deleter] =
+                    it->second.first(std::make_tuple(std::forward<Args>(args)...));
                 return std::unique_ptr<Base, DeleterFunc>(static_cast<Base*>(raw_ptr), deleter);
             }
             throw std::logic_error("Type not registered.");
+        }
+
+        template <typename Service, typename Impl, typename... Args>
+        void RegisterService(Args&&... args) {
+            Register<Service, Impl, Args...>();
+            ResetService<Service>(std::forward<Args>(args)...);
+        }
+
+        template <typename Service, typename... Args>
+        void ResetService(Args&&... args) {
+            auto it = _factories.find(typeid(Service));
+            if (it != _factories.end()) {
+                auto& [factory, service] = it->second;
+                auto [raw_ptr, deleter]  = factory(std::make_tuple(std::forward<Args>(args)...));
+                std::unique_ptr<void, DeleterFunc> new_service(raw_ptr, deleter);
+                service.swap(new_service);
+            } else {
+                throw std::logic_error("Service not registered.");
+            }
+        }
+
+        template <typename Service>
+        std::unique_ptr<Service, DeleterFunc>& Get() {
+            auto it = _factories.find(typeid(Service));
+            if (it != _factories.end()) {
+                auto& [factory, service] = it->second;
+                if (service) {
+                    return reinterpret_cast<std::unique_ptr<Service, DeleterFunc>&>(service);
+                }
+                throw std::logic_error("Service not created.");
+            }
+            throw std::logic_error("Service not registered.");
         }
     };
 }
